6f63a5733b447af42055b96f8cdf2a50564d6a58
Chandnim0712 <mchandni927@gmail.com>
2025-06-22 00:29:53 +0530

    Initial commit - Spring Boot E-Commerce project

---
+++ LoginPage/src/main/java/com/DB/DBConnect.java
@@ -0,0 +1,30 @@
+package com.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+
+	private static final String URL = "jdbc:postgresql://localhost:5432/backend";
+	private static final String USER = "postgres";
+	private static final String PASSWORD = "admin";
+
+	private static Connection conn = null;
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+
+			if (conn == null) {
+				Class.forName("org.postgresql.Driver");
+				conn = DriverManager.getConnection(URL, USER, PASSWORD);
+				System.out.println("Connected to PostgreSQL successfully.");
+			}
+		} catch (Exception e) {
+			System.out.println("Connection Failed.");
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+}
